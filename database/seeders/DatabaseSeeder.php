f74833f7b3a63015cc29ac42bed14a42b4e20a45
IVANESEN <20245200@esen.edu.sv>
2026-02-19 07:38:56 -0600

    nuevoCambio

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,4 +20,22 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
     }
+    public function run(): void
+    {
+        $csvFile = database_path('data/libros.csv');
+        if (file_exists($csvFile)) {
+            $handle = fopen($csvFile, 'r');
+            fgetcsv($handle);
+            while (($data = fgetcsv($handle)) !== FALSE) {
+                \App\Models\Book::create([
+                    'title' => $data[0],
+                    'description' => $data[1],
+                    'isbn' => $data[2],
+                    'total_copies' => (int)$data[3],
+                    'available_copies' => (int)$data[4],
+                    'status' => strtolower($data[5]) === 'disponible',
+                ]);
+            }
+            fclose($handle);
+        }
 }
